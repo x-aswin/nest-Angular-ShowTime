bef7ff4a1e82ef606253dfadf0f055271973bd75
Aswin <aswin102004@gmail.com>
2026-04-03 14:13:00 +0530

    navbar changes

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -3,7 +3,7 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
